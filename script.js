9c4f2629b5f1ca3e81cf77b3335530ce9a6d5774
CarlosMS12 <carlosmagallanes207@gmail.com>
2022-11-28 14:52:14 -0500

    interaccion entre todos los componentes

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const productDetailCloseIcon = document.querySelector(".product-detail-close")
 const shoppingCartContainer = document.querySelector("#shoppingCartContainer")
+const productDetailContainer = document.querySelector("#productDetail")
 const cardsContainer = document.querySelector(".cards-container")
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside)
+productDetailCloseIcon.addEventListener("click", closeProductDetailAside)
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains("inactive")
@@ -26,6 +29,9 @@ function toggleMobileMenu() {
         
         shoppingCartContainer.classList.add("inactive");
     }
+
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle("inactive");
 }
 
@@ -36,10 +42,27 @@ function toggleCarritoAside() {
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add("inactive")
     }
+
+    const isProductDetailClosed = productDetailContainer.classList.contains("inactive")
+
+
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add("inactive")
+    }
+
     shoppingCartContainer.classList.toggle("inactive");
     
 
     
+}
+
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add("inactive")
+
+    productDetailContainer.classList.remove("inactive")
+}
+function closeProductDetailAside() {
+    productDetailContainer.classList.add("inactive")
 }
 
 const productList = [];
@@ -70,6 +93,7 @@ function renderProducts(arr) {
     
         const productImg = document.createElement("img")
         productImg.setAttribute("src", product.imagen)
+        productImg.addEventListener("click", openProductDetailAside)
     
         const productInfo = document.createElement("div")
         productInfo.classList.add("product-info")
